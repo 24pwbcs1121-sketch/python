9309084bd1bb1a04f8959170836f5ba9332f5c5a
Eatisam Alam <24pwbcs1121@uetpeshawar.edu.pk>
2025-09-20 23:56:07 +0500

    Grading System

---
+++ Grading_system.py
@@ -0,0 +1,13 @@
+
+marks=int(input("Enter Your Marks :"))
+if(marks>=90):
+    print("You have got A Grade")
+elif(marks<90 and marks>=80):
+    print("You have got B Grade")
+elif(marks<80 and marks>=70):
+    print("You have got C Grade")
+elif(marks<70 and marks>=60):
+    print("You have got D Grade")
+else:
+    print("You have got F Grade ")
+
