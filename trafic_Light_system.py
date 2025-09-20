8406ea4e108a0d6795dd51b03a6bbdee143b03ba
Eatisam Alam <24pwbcs1121@uetpeshawar.edu.pk>
2025-09-20 23:49:42 +0500

    This code is about traffic light system

---
+++ trafic_Light_system.py
@@ -0,0 +1,9 @@
+light=input("Light color : ")
+if(light=="red"):
+    print("stop ")
+elif(light=="yellow"):
+    print("look ")
+elif(light=="green"):
+    print("go ")
+else:
+    print("Light is broken ")
